3d52ead9139f72859f1b637b0eaf41cf3c034577
Pucukio <pucuk4us@gmail.com>
2023-06-04 15:41:35 +0700

    Update Permission and AndroidEntryPoint

--- app/src/main/java/com/pucuk/binar_challenge_ch_7/MainActivity.kt
+++ app/src/main/java/com/pucuk/binar_challenge_ch_7/MainActivity.kt
@@ -4,11 +4,13 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.ViewGroup
 import android.widget.Button
+import dagger.hilt.android.AndroidEntryPoint
 
+
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
     }
 }
