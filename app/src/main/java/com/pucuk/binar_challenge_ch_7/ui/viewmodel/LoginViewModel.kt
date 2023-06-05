d9efe8f3a2ae32e49c132a09fa95355b56d0cd70
Pucukio <pucuk4us@gmail.com>
2023-06-05 23:26:27 +0700

    Updated buildFlavoring

--- app/src/main/java/com/pucuk/binar_challenge_ch_7/ui/viewmodel/LoginViewModel.kt
+++ app/src/main/java/com/pucuk/binar_challenge_ch_7/ui/viewmodel/LoginViewModel.kt
@@ -6,6 +6,7 @@ import androidx.lifecycle.ViewModel
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
+import com.google.firebase.auth.FirebaseAuth
 import com.pucuk.binar_challenge_ch_7.data.network.ApiClient
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
